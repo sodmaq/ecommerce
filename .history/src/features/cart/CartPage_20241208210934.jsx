d421abb9890027892b1339fbb0335df495e9caaf
sodmaq <sodmaq@gmail.com>
2024-12-08 21:16:08 +0100

    order and clear cart func

--- .history/src/features/cart/CartPage_20241208210934.jsx
+++ .history/src/features/cart/CartPage_20241208210934.jsx
@@ -27,7 +27,7 @@ function CartPage() {
             </button>
             <button
               className="mt-4 bg-red-500 text-white py-2 px-4 rounded"
-              onClick={() => navigate("/order")}
+              onClick={() => navigate("/")}
             >
               order now
             </button>
