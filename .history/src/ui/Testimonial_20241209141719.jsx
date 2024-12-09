3111ad5c9c688a51cbdd2191a9e54578c7e689d7
sodmaq <sodmaq@gmail.com>
2024-12-09 14:24:10 +0100

    testimonial refactor

---
+++ .history/src/ui/Testimonial_20241209141719.jsx
@@ -0,0 +1,235 @@
+import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa";
+
+function Testimonial() {
+  return (
+    <div>
+      <div className="my-6 font-[sans-serif] max-w-6xl mx-auto">
+        <div className="max-w-2xl mx-auto text-center">
+          <h2 className="text-3xl font-extrabold text-gray-800">
+            What our happy client say
+          </h2>
+          <p className="text-sm mt-4 leading-relaxed text-gray-800">
+            Veniam proident aute magna anim excepteur et ex consectetur velit
+            ullamco veniam minim aute sit. Elit occaecat officia et laboris
+            Lorem minim. Officia do aliqua adipisicing ullamco in
+          </p>
+        </div>
+
+        <div className="grid md:grid-cols-3 gap-6 max-md:gap-12 max-md:justify-center text-center max-md:max-w-lg mx-auto mt-16">
+          <div className="rounded-md">
+            <div className="flex flex-col items-center">
+              <img
+                src="/john doe.webp"
+                className="w-24 h-24 rounded-full shadow-xl border-2 border-white"
+              />
+              <div className="mt-4">
+                <h4 className="text-sm font-extrabold text-gray-800">
+                  John Doe
+                </h4>
+                <p className="text-xs text-blue-600 font-bold mt-1">
+                  CEO, Company
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <p className="text-sm leading-relaxed text-gray-800">
+                The service was amazing. I never had to wait that long for my
+                food. The staff was friendly and attentive, and the delivery was
+                impressively prompt.
+              </p>
+            </div>
+
+            <div className="flex justify-center space-x-1 mt-4">
+              <FaStar className="w-7  text-blue-600" />
+              <CiStar className="w-4 fill-[#007bff]" />
+              <CiStar className="w-4 fill-[#007bff]" />
+              <CiStar className="w-4 fill-[#007bff]" />
+              <CiStar className="w-4 fill-[#007bff]" />
+
+              {/* <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#CED5D8]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#CED5D8]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg> */}
+            </div>
+          </div>
+
+          <div className="rounded-md">
+            <div className="flex flex-col items-center">
+              <img
+                src="/mark.webp"
+                className="w-24 h-24 rounded-full shadow-xl border-2 border-white"
+              />
+              <div className="mt-4">
+                <h4 className="text-sm font-extrabold text-gray-800">
+                  Mark Adair
+                </h4>
+                <p className="text-xs text-blue-600 font-bold mt-1">
+                  CEO, Company
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <p className="text-sm leading-relaxed text-gray-800">
+                The service was amazing. I never had to wait that long for my
+                food. The staff was friendly and attentive, and the delivery was
+                impressively prompt.
+              </p>
+            </div>
+
+            <div className="flex justify-center space-x-1 mt-4">
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+            </div>
+          </div>
+
+          <div className="rounded-md">
+            <div className="flex flex-col items-center">
+              <img
+                src="/simon.webp"
+                className="w-24 h-24 rounded-full shadow-xl border-2 border-white"
+              />
+              <div className="mt-4">
+                <h4 className="text-sm font-extrabold text-gray-800">
+                  Simon Konecki
+                </h4>
+                <p className="text-xs text-blue-600 font-bold mt-1">
+                  CEO, Company
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <p className="text-sm leading-relaxed text-gray-800">
+                The service was amazing. I never had to wait that long for my
+                food. The staff was friendly and attentive, and the delivery was
+                impressively prompt.
+              </p>
+            </div>
+
+            <div className="flex justify-center space-x-1 mt-4">
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#CED5D8]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial;
--- .history/src/ui/Testimonial_20241209141951.jsx
+++ .history/src/ui/Testimonial_20241209141951.jsx
@@ -47,6 +47,47 @@ function Testimonial() {
               <FaStar className="w-7  text-blue-600" />
               <CiStar className="w-4 fill-[#007bff]" />
               <CiStar className="w-4 fill-[#007bff]" />
+
+              {/* <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#007bff]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#CED5D8]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                className="w-4 fill-[#CED5D8]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg> */}
             </div>
           </div>
 
